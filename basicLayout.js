b9b2a0d47e6dec49825df69d8bf0ff18676e96f8
Driijko <driijko@gmail.com>
2022-06-14 15:06:24 -0400

    Test title splash on mobile.

--- basicLayout.js
+++ basicLayout.js
@@ -1,3 +1,7 @@
+let urw;
+let urh;
+let pageLayout;
+
 function layout () {
   // document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
 
@@ -8,8 +12,10 @@ function layout () {
     if (aspectRatio < 1.777) {
       let width = ((0.5625 * window.innerHeight) / window.innerWidth) * window.innerWidth;
       document.documentElement.style.setProperty("--w", width);
+      urw = width;
       document.documentElement.style.setProperty("--wpx", `${width}px`);
       document.documentElement.style.setProperty("--h", window.innerHeight);
+      urh = window.innerHeight;
       document.documentElement.style.setProperty("--gh", window.innerHeight +1);
       document.documentElement.style.setProperty("--gw", ((window.innerWidth - width)/2) +1);
       document.documentElement.style.setProperty("--vg", "none");
@@ -17,7 +23,9 @@ function layout () {
     }
     else {
       document.documentElement.style.setProperty("--h", window.innerWidth * 1.777);
+      urh = window.innerWidth * 1.777;
       document.documentElement.style.setProperty("--w", window.innerWidth);
+      urw = window.innerWidth;
       document.documentElement.style.setProperty("--wpx", `${window.innerWidth}px`);
       document.documentElement.style.setProperty("--gh", ((window.innerHeight - (window.innerWidth * 1.777))/2) +1);
       document.documentElement.style.setProperty("--gw", window.innerWidth + 1);
@@ -25,13 +33,16 @@ function layout () {
       document.documentElement.style.setProperty("--hg", "none");
     };
     document.documentElement.style.setProperty("--orientation", "portrait");
+    pageLayout = "portrait";
   }
   else {
     let aspectRatio = window.innerWidth / window.innerHeight;
     if(aspectRatio < 1.6) {
       let height = ((0.625 * window.innerWidth) / window.innerHeight) * window.innerHeight;
       document.documentElement.style.setProperty("--h", height);
+      urh = height;
       document.documentElement.style.setProperty("--w", window.innerWidth);
+      urw = window.innerWidth;
       document.documentElement.style.setProperty("--wpx", `${window.innerWidth}px`);
       document.documentElement.style.setProperty("--gh", ((window.innerHeight - height)/2) +1);
       document.documentElement.style.setProperty("--gw", (window.innerWidth) +1);
@@ -41,7 +52,9 @@ function layout () {
     else {
       let width = window.innerHeight * 1.6;
       document.documentElement.style.setProperty("--h", window.innerHeight);
+      urh = window.innerHeight;
       document.documentElement.style.setProperty("--w", width);
+      urw = width;
       document.documentElement.style.setProperty("--wpx", `${width}px`);
       document.documentElement.style.setProperty("--gh", window.innerHeight +1);
       document.documentElement.style.setProperty("--gw", ((window.innerWidth - width)/2) +1);
@@ -49,20 +62,19 @@ function layout () {
       document.documentElement.style.setProperty("--hg", "block");
     }
     document.documentElement.style.setProperty("--orientation", "landscape");
+    pageLayout = "landscape";
   }
 }
 
 layout();
 
-function resize() {
-  resizeReady = true;
-  layout();
-}
-
-let resizeReady = true;
+let numOfResizes = 0;
 window.addEventListener("resize", ()=> {
-  if(resizeReady) {
-    resizeReady = false;
-    setTimeout(resize, 2000);
-  }
+  numOfResizes += 1;
+  const thisResizeNum = numOfResizes;
+  const timerId = setTimeout(()=> {
+    if (numOfResizes === thisResizeNum) {
+      location.reload();
+    }
+  }, 2000);
 });
--- geometry/geometry.js
+++ geometry/geometry.js
@@ -34,12 +34,25 @@ const nameToNum = {
 let currentContent = 0;
 
 gsap.set(".layer", {pointerEvents: "none",});
-gsap.set(".layer-background", {transformOrigin: "0% 0%"});
-gsap.set([".layer-content", "#page-background", "#title", "#menu-layer"], {
+gsap.set([".layer-background", "#menu-toggle"], {transformOrigin: "0% 0%"});
+gsap.set([".layer-content", "#page-background", "#title"], {
   opacity: 0,
 });
 gsap.set("#menu-layer", {zIndex:0});
 gsap.set("#title .content-layer-link", {display: "none"});
+
+// TITLE ----------------------------------------------------
+gsap.set(".l1-paths1", {transformOrigin: "center", scale: 0.1});
+gsap.set("#title h2", {opacity: 0});
+if (pageLayout === "landscape") {
+  gsap.set(".layer-content svg", {
+    // transform: "scale(0.5)",
+  });
+};
+
+// MENU -----------------------------------------------------
+gsap.set(["#menu-layer .layer-background rect","#menu-toggle"], {opacity: 0});
+
 
 const tlInitial = gsap.timeline();
 contentOptions(0, tlInitial);
@@ -92,38 +105,34 @@ function contentOptionSelected(layer) {
   contentCurrent(layer, tl);
 }
 
-function contentCurrent(layer, tl) {
+function contentCurrent(layer, tl, pos) {
   tl.set(layer.name, {
     zIndex: 2,
-  });
+  }, pos);
   if (layer.contentEntrance) {
     genericEntrance(`${layer.name} .layer-content`, tl, 0);
   } else {
-    layer.specialEntrance(tl);
+    layer.specialEntrance(tl, pos);
     layer.contentEntrance = true;
   };
-  tl.set("#menu-layer", {zIndex: 3});
-  tl.to("#menu-layer", {duration: 2, opacity: 1, ease: "power2.in"});
 };
 
 // MENU CONFIGURATIONS ---------------------------------------------
 function menuConfig1(tl, duration) {
   tl.to("#menu-layer .layer-background", {
-    transform: "scale(0.3, 0.5)",
-    left: "35%",
-    top: "30%",
+    transform: "scale(0.25, 0.13)",
+    x: "38%",
+    y: "75%",
     duration: duration,
   });
   tl.to("#menu-toggle", {
-    backgroundColor: "red",
-    left: "40%",
-    top: "50%", 
+    x: urw * 0.43,
+    y: urh * 0.77, 
     duration: duration,
   }, "<0");
   tl.to("#menu-checkbox-label", {
-    backgroundColor: "white",
+    scale: 0.7,
     duration: duration,
-    transform: "scale(0.9)",
   }, "<0");
 };
 
@@ -131,14 +140,14 @@ function menuConfig2(tl, duration) {
   tl.to("#menu-layer .layer-background", {
     transform: "scale(1)",
     duration: duration,
-    left: "0%",
-    top: "0%", 
+    x: "0%",
+    y: "0%", 
     pointerEvents: "initial",
   });
   tl.to("#menu-toggle", {
     backgroundColor: "purple",
-    left: "40%",
-    top: "80%",
+    x: "40%",
+    y: "80%",
     duration: duration,
   }, "<0");
   tl.to("#menu-checkbox-label", {
@@ -152,8 +161,8 @@ function menuConfig2(tl, duration) {
 function introductionOptionSelect(tl) {
   tl.to("#introduction .layer-background", {
     transform: "scale(1)",
-    left: "0%",
-    top: "0%",
+    x: "0%",
+    y: "0%",
   });
 }
 
@@ -190,44 +199,84 @@ function hideContentOptions(tl) {
 };
 
 // CONTENT LAYERS ----------------------------------------------
-function titleContentEntrance(tl) {
+function titleContentEntrance(tl, pos) {
+  tl.set("#menu-layer", {zIndex: 3}, pos);
   tl.to("#title .layer-content", {
     opacity: 1,
     duration: 3,
-  });
-  tl.to("#title .layer-content h1", {
-    color: "red",
-    duration: 2,
-  });
-}
+  }, pos);
+  tl.to(".l1-paths1", {
+    stroke: "hsl(0, 0%, 0%)",
+    duration: 11,
+    ease: "power1.out",
+    scale: 1,
+    fill: "hsla(0, 0%, 0%, 0.25)",
+  }, pos);
+  tl.to("#l1-path1", {
+    rotation: 360,
+    duration: 11,
+    ease: "power1.out",
+  }, pos);
+  tl.to("#l1-path2", {
+    rotation: -360,
+    duration: 11,
+    ease: "power1.out",
+  }, pos);
+  tl.to("#l1-path3", {
+    rotation: 1800,
+    duration: 11,
+    ease: "power1.out",
+  }, pos);
+  tl.to("#title h2", {
+    opacity: 1,
+    duration: 5,
+    ease: "power1.in"
+  }, pos + 7);
+  tl.to(["#menu-layer .layer-background rect"], {
+    opacity: 1,
+    duration: 1,
+    ease: "none"
+  }, pos + 11);
+  tl.to(["#menu-toggle"], {
+    opacity: 1,
+    duration: 3,
+    ease: "power1.in"
+  }, pos + 12);
+  tl.to("#m-rec1", {
+    fill: "hsl(0, 0%, 4%)",
+    duration: 1,
+    ease: "power1.in"
+  }, pos + 12);
+};
+
+
 
 // EVENTS //////////////////////////////////////////////
 // PAGE LOAD & SPLASH ----------------------------------------
-// let loadingScreenDelayed = false;
+let loadingScreenDelayed = false;
 
-// const timerId1 = setTimeout(()=> {
-//   loadingScreenDelayed = true;
-//   clearTimeout(timerId1);
-// },2000);
+const timerId1 = setTimeout(()=> {
+  loadingScreenDelayed = true;
+  clearTimeout(timerId1);
+},2000);
 
-// function transitionToSplash() {
-//   const tl = gsap.timeline();
-//   tl.to("#page-loading-screen", {duration: 1, opacity: 0,}); 
-//   tl.set("#page-loading-screen", {display: "none"});
-//   tl.to(["#page-background","#title"], {duration: 2, opacity: 1,});
-//   contentCurrent(contentLayers[0], tl);
-// }
+function transitionToSplash() {
+  const tl = gsap.timeline();
+  tl.to(["#page-background","#title"], {duration: 2, opacity: 1,}, 0);
+  tl.set("#page-loading-screen", {display: "none"}, 2);
+  contentCurrent(contentLayers[0], tl, 2);
+}
 
-// window.addEventListener("load",()=> {
-//   if (loadingScreenDelayed) {
-//     transitionToSplash();
-//   } else {
-//     const timerId2 = setTimeout(()=> {
-//       transitionToSplash();
-//       clearTimeout(timerId2);
-//     }, 1000);
-//   }
-// });
+window.addEventListener("load",()=> {
+  if (loadingScreenDelayed) {
+    transitionToSplash();
+  } else {
+    const timerId2 = setTimeout(()=> {
+      transitionToSplash();
+      clearTimeout(timerId2);
+    }, 1000);
+  }
+});
 
 // function postSplash() {
 //   gsap.set("#page-loading-screen", {opacity: 0,}); 
@@ -235,12 +284,17 @@ function titleContentEntrance(tl) {
 //   gsap.set(["#page-background","#title"], {opacity: 1,});
 //   gsap.set("#title", {zIndex: 2 });
 //   gsap.set("#title .layer-content", {opacity: 1});
-//   gsap.set("#title .layer-content h1", {color: "red"});
-//   gsap.set("#menu-layer", {zIndex: 3});
-//   gsap.set("#menu-layer", {opacity: 1});
+//   gsap.set("#menu-layer", {zIndex: 3, opacity: 1});
+//   gsap.set(["#menu-layer .layer-background rect","#menu-toggle"], {opacity: 0});
 // }
 
 // postSplash();
+
+// const tl = gsap.timeline();
+// tl.to(".gutter", {backgroundColor: "blue", duration: 5});
+// contentCurrent(contentLayers[0], tl, 5);
+// titleContentEntrance(tl);
+// tl.progress(1);
 
 // MENU CHECKBOX CHECKED / UNCHECKED ---------------------------------
 document.getElementById("menu-checkbox").addEventListener("change", (e)=> {
@@ -259,4 +313,4 @@ for (const link of document.getElementsByClassName("content-layer-link")) {
   link.addEventListener("click", (e)=> {
     contentOptionSelected(contentLayers[nameToNum[e.target.dataset.layer]])
   });
-}
+};
